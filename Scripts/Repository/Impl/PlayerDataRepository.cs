6fb001c822117a54230d0709bba1a41038549e7f
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-01-10 15:26:35 -0500

    Added player data to repository class.

--- Scripts/Repository/Impl/PlayerDataRepository.cs
+++ Scripts/Repository/Impl/PlayerDataRepository.cs
@@ -1,3 +1,13 @@
+using System.Collections.Generic;
 using RepositorySystem;
 
-public class PlayerDataRepository : IRepository { }
+public class PlayerDataRepository : IRepository {
+    public Dictionary<Ingredient, int> Ingredients { get; } = new();
+    public Dictionary<Ingredient, int> StoreInventory { get; } = new();
+    public HashSet<RecipeName> DiscoveredRecipes { get; } = [];
+    public HashSet<RecipeName> AnimalLikes { get; } = [];
+    public HashSet<RecipeName> AnimalDislikes { get; } = [];
+    public int AnimalHealth { get; set; } = 0;
+    public int AnimalHealthRequired { get; } = 100;
+    public RecipeName? CookedDish { get; set; } = null;
+}
