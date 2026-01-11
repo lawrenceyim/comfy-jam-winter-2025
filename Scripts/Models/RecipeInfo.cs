87066ec6856a42e71a4846c560205e792e48ba97
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-01-11 14:27:11 -0500

    Added function to find recipe by ingredients.

--- Scripts/Models/RecipeInfo.cs
+++ Scripts/Models/RecipeInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 public static class RecipeInfo {
     private static readonly Dictionary<RecipeName, Recipe> _recipes = new() {
         {
-            RecipeName.Dish, new Recipe(RecipeName.Dish, TextureId.Dish, new Dictionary<IngredientName, int>() {
+            RecipeName.Mistake, new Recipe(RecipeName.Mistake, TextureId.Dish, new Dictionary<IngredientName, int>() {
                 { IngredientName.Egg, 2 }
             })
         }
@@ -12,10 +12,38 @@ public static class RecipeInfo {
     public static Recipe GetRecipe(RecipeName recipeName) {
         return _recipes[recipeName];
     }
+
+    public static RecipeName FindRecipeByIngredients(Dictionary<IngredientName, int> ingredients) {
+        foreach (KeyValuePair<RecipeName, Recipe> recipe in _recipes) {
+            bool match = true;
+            Dictionary<IngredientName, int> recipeIngredients = recipe.Value.Ingredients;
+
+            if (ingredients.Count != recipeIngredients.Count) {
+                continue;
+            }
+
+            foreach (KeyValuePair<IngredientName, int> ingredient in ingredients) {
+                if (
+                    !recipeIngredients.ContainsKey(ingredient.Key) ||
+                    recipeIngredients[ingredient.Key] != ingredient.Value
+                ) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                return recipe.Key;
+            }
+        }
+
+        return RecipeName.Mistake;
+    }
 }
 
 public record Recipe(RecipeName Name, TextureId TextureId, Dictionary<IngredientName, int> Ingredients);
 
 public enum RecipeName {
-    Dish
+    Mistake = 0,
+    ValidDish1 = 1,
 }
