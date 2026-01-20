7d6b94e85e9ceb45f0681c247b31cd29b069a36c
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-01-20 15:49:01 -0500

    Added carpet to living room.

--- .godot/mono/temp/obj/Debug/comfy-jam-winter-2025.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/comfy-jam-winter-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("comfy-jam-winter-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4391f69968357e8f7a21b300656941a0f3fe6d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba215f4548a6d2941a4ce310258134bb619025f")]
 [assembly: System.Reflection.AssemblyProductAttribute("comfy-jam-winter-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("comfy-jam-winter-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
