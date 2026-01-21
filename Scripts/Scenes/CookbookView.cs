0f7edfc11e935ae6d795169084e41f86ff19e291
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-01-21 09:19:49 -0500

    Added ingredient display for cookbook.

--- Scripts/Scenes/CookbookView.cs
+++ Scripts/Scenes/CookbookView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 using RepositorySystem;
 using ServiceSystem;
@@ -11,21 +12,53 @@ public partial class CookbookView : Node2D {
 
     private TextureRepository _textureRepository;
 
-    public override void _Ready() {
-        DisplaySelectedRecipe(false);
+    private static readonly Vector2 _ingredientCardScale = new Vector2(.065f, .065f);
+    private Sprite2D[] _ingredientCards = new Sprite2D[6];
+    private Vector2 _ingredientCardSize = new Vector2(1852, 2516) * _ingredientCardScale;
+    private Vector2 _ingredientCardOffset = new(10, 25);
+    private Vector2 _ingredientCardStartPosition = new(720, 250);
 
+    public override void _Ready() {
         ServiceLocator serviceLocator = GetNode<ServiceLocator>(ServiceLocator.AutoloadPath);
         RepositoryLocator repositoryLocator = serviceLocator.GetService<RepositoryLocator>();
         _textureRepository = repositoryLocator.GetRepository<TextureRepository>(RepositoryName.Texture);
 
-        
+        for (int i = 0; i < _ingredientCards.Length; i++) {
+            Sprite2D sprite = new Sprite2D();
+            AddChild(sprite);
+            _ingredientCards[i] = sprite;
+
+            int column = i % 3;
+            int row = i / 3;
+
+            float x = _ingredientCardStartPosition.X
+                      + column * (_ingredientCardSize.X + _ingredientCardOffset.X);
+
+            float y = _ingredientCardStartPosition.Y
+                      + row * (_ingredientCardSize.Y + _ingredientCardOffset.Y);
+
+            sprite.Position = new Vector2(x, y);
+            sprite.Scale = _ingredientCardScale;
+        }
+
         // TODO: Remove test
+        SetSelectedRecipe(RecipeName.SteakFrite);
         DisplaySelectedRecipe(true);
-        SetSelectedRecipe(RecipeName.FrenchFries);
     }
 
     public void DisplaySelectedRecipe(bool visible) {
         _selectedRecipeSprite.Visible = visible;
+        foreach (Sprite2D sprite in _ingredientCards) {
+            sprite.Visible = visible;
+        }
+    }
+
+    public void ClearSelectedRecipe() {
+        _selectedRecipeLabel.Text = string.Empty;
+        _selectedRecipeSprite.Texture = null;
+        foreach (Sprite2D sprite in _ingredientCards) {
+            sprite.Texture = null;
+        }
     }
 
     public void SetSelectedRecipe(RecipeName recipeName) {
@@ -48,8 +81,9 @@ public partial class CookbookView : Node2D {
 
         _selectedRecipeSprite.Texture = _textureRepository.GetTexture(textureId);
 
-        // TODO: Set ingredients
+        int ingredientIndex = 0;
+        foreach (KeyValuePair<IngredientName, int> kvp in recipe.Ingredients) {
+            _ingredientCards[ingredientIndex++].Texture = _textureRepository.GetTexture(IngredientUtil.GetTextureId(kvp.Key));
+        }
     }
-
-    // make 3 by 2 card for ingredients
 }
