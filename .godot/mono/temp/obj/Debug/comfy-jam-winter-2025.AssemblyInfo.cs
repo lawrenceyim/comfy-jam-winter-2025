230c6d5da3200c9cd8f884449632c484bddba5d1
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-01-23 09:47:43 -0500

    Added 2D movement for animal.

--- .godot/mono/temp/obj/Debug/comfy-jam-winter-2025.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/comfy-jam-winter-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("comfy-jam-winter-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfa97ea0ff49c34371ce6f95b2b08778d768398")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6868aa10865c28f85a04287df4f1250e9554a22e")]
 [assembly: System.Reflection.AssemblyProductAttribute("comfy-jam-winter-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("comfy-jam-winter-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Scripts/Game Objects/AnimalPlayable.cs
+++ Scripts/Game Objects/AnimalPlayable.cs
@@ -1,7 +1,73 @@
 using Godot;
-using System;
 
 public partial class AnimalPlayable : CharacterBody2D {
     [Export]
     private AnimatedSprite2D _sprite;
+
+    private enum PlayerFacing {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    private PlayerFacing _playerFacing = PlayerFacing.Down;
+
+    private const string IdleDown = "Idle Down";
+    private const string IdleSide = "Idle Side";
+    private const string IdleUp = "Idle Up";
+    private const string WalkDown = "Walk Down";
+    private const string WalkSide = "Walk Side";
+    private const string WalkUp = "Walk Up";
+
+    private float _speed = 250f;
+    private Vector2 _movement = Vector2.Zero;
+
+    public override void _Process(double delta) {
+        _Move(delta);
+        _PlayAnimation();
+    }
+
+    private void _Move(double delta) {
+        _movement = Vector2.Zero;
+
+        if (Input.IsKeyPressed(Key.W)) {
+            _movement.Y -= 1;
+        }
+
+        if (Input.IsKeyPressed(Key.S)) {
+            _movement.Y += 1;
+        }
+
+        if (Input.IsKeyPressed(Key.A)) {
+            _movement.X -= 1;
+        }
+
+        if (Input.IsKeyPressed(Key.D)) {
+            _movement.X += 1;
+        }
+
+        _movement = _movement.Normalized();
+        MoveAndCollide(_movement * (float)delta * _speed);
+    }
+
+    private void _PlayAnimation() {
+        if (_movement == Vector2.Zero) {
+            _sprite.Play(_playerFacing switch {
+                PlayerFacing.Up => IdleUp,
+                PlayerFacing.Down => IdleDown,
+                PlayerFacing.Left => IdleSide,
+                PlayerFacing.Right => IdleSide,
+                _ => IdleDown
+            });
+            return;
+        }
+
+        if (_movement.X != 0) {
+            _sprite.FlipH = _movement.X < 0;
+            _sprite.Play(WalkSide);
+        } else {
+            _sprite.Play(_movement.Y > 0 ? WalkDown : WalkUp);
+        }
+    }
 }
